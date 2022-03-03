95d90d8aacf7bdb7df4146bdb7af2746a9c31a42
Dan Page <dan@phoo.org>
2022-03-03 10:42:34 +0000

    typo: demo -> password

--- src/challenge/password/build.py
+++ src/challenge/password/build.py
@@ -7,18 +7,18 @@
 import math, random, string, libbuild.util as util
 
 def apply_fixups( args, conf ) :
-  if   ( conf.get( 'alphabet', 'demo' ) == 'binary' ) :
-    conf.set( 'alphabet', 0, 'demo' )
-  elif ( conf.get( 'alphabet', 'demo' ) == 'ascii'  ) :
-    conf.set( 'alphabet', 1, 'demo' )
+  if   ( conf.get( 'alphabet', 'password' ) == 'binary' ) :
+    conf.set( 'alphabet', 0, 'password' )
+  elif ( conf.get( 'alphabet', 'password' ) == 'ascii'  ) :
+    conf.set( 'alphabet', 1, 'password' )
 
 def build_params( args, conf ) :
-  len_P = random.randint( int( conf.get( 'len_P_min', 'demo' ) ),
-                          int( conf.get( 'len_P_max', 'demo' ) ) )
+  len_P = random.randint( int( conf.get( 'len_P_min', 'password' ) ),
+                          int( conf.get( 'len_P_max', 'password' ) ) )
 
-  if   ( conf.get( 'alphabet', 'demo' ) == 'binary' ) :
+  if   ( conf.get( 'alphabet', 'password' ) == 'binary' ) :
     P = util.bytes_rand( len_P                                                ) + bytes( '\x00'.encode( 'ascii' ) )
-  elif ( conf.get( 'alphabet', 'demo' ) == 'ascii'  ) :
+  elif ( conf.get( 'alphabet', 'password' ) == 'ascii'  ) :
     P = util.bytes_rand( len_P, xs = string.ascii_lowercase.encode( 'ascii' ) ) + bytes( '\x00'.encode( 'ascii' ) )
 
   return [ ( 'P', P, False ) ]
